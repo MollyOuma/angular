6166e56c3e97aec1382eeb792cfa7086eaf1ed0b
MollyOuma <princessmolly991@gmail.com>
2022-10-18 11:18:59 +0300

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TaxCalculator';
+export class AppComponent implements OnInit {
+  //title = 'TaxCalculator';
+
+  public users: User[] = [];
+
+  constructor(private UserService: UserService){ }
+
+  ngOnInit(): void {
+      this.getUsers();
+  }
+
+
+public getUsers(): void {
+  this.UserService.getUsers().subscribe((response: User[]) => {
+    this.users = response;
+  },
+  (error: HttpErrorResponse) => {
+    alert(error.message)
+  }
+  ); 
 }
+} 
+
+
+
